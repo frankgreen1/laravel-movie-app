40689694be50f731b1bedcf99d66957b14e047ca
greenfrank1 <greenfrank010@gmail.com>
2021-02-24 07:52:17 +0100

    modified the model on the show blade

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -91,49 +91,49 @@
             </div>
      </div> <!--end of cast section-->
      <div class="movie-images" x-data="{ isOpen: false, image: ''}">
-         <div class="container max-auto px-4 py-16">
-             <h2 class="text-4xl font-semibold text-gold-500">Images</h2>
-             <div class="grid grid-cols-1 sm:grid-cols-2 md:cols-3 gap-8">
-                 @foreach ($show['images']['backdrops'] as $image)
-                        @if ($loop->index < 8)
-                            <div class="mt-8">
-                                <a
-                                @click.prevent=
-                                "isOpen = true
-                                image='{{'https://images.tmdb.org/t/p/original/'.$image['file_path']}}'
-                                "
-                                  href="#"
-                                >
-                                    <img src="{{ 'https://images.tmdb.org/t/p/w500/'.$image['file_path']}}" alt="" class="hover;opacity-75 trainsition ease-in-out duration-150">
-                                </a>
-                            </div>
-                        @endif
-                 @endforeach
-             </div>
-                            <div
-                                style="background-color: rgba(0, 0, 0, .5);"
-                                class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
-                                x-show ="isOpen"
-                            >
-                                <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
-                                    <div class="bg-gray-900 rounded">
-                                        <div class="flex justify-end pr-4 pt-2">
-                                            <button
-                                            @click="isOpen = false"
-                                                @keydown.escape.window="isOpen = false"
-                                                class="text-3xl leading-none hover:text-gray-300">&times;
-                                            </button>
-                                        </div>
-                                        <div class="modal-body px-8 py-8">
+        <div class="container max-auto px-4 py-16">
+            <h2 class="text-4xl font-semibold text-gold-500">Images</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:cols-3 gap-8">
+                @foreach ($show['images']['backdrops'] as $image)
+                       @if ($loop->index < 8)
+                           <div class="mt-8">
+                               <a
+                               @click.prevent=
+                               "isOpen = true
+                               image='{{'https://images.tmdb.org/t/p/original/'.$image['file_path']}}'
+                               "
+                                 href="#"
+                               >
+                                   <img src="{{ 'https://images.tmdb.org/t/p/w500/'.$image['file_path']}}" alt="" class="hover;opacity-75 trainsition ease-in-out duration-150">
+                               </a>
+                           </div>
+                       @endif
+                @endforeach
+            </div>
+                           <div
+                               style="background-color: rgba(0, 0, 0, .5);"
+                               class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                               x-show ="isOpen"
+                           >
+                               <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                                   <div class="bg-gray-900 rounded">
+                                       <div class="flex justify-end pr-4 pt-2">
+                                           <button
+                                           @click="isOpen = false"
+                                               @keydown.escape.window="isOpen = false"
+                                               class="text-3xl leading-none hover:text-gray-300">&times;
+                                           </button>
+                                       </div>
+                                       <div class="modal-body px-8 py-8">
 
-                                                <img :src="image" alt="poster">
+                                               <img :src="image" alt="poster">
 
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-         </div>
-     </div>  <!-- end movie-images -->
+                                       </div>
+                                   </div>
+                               </div>
+                           </div>
+        </div>
+    </div>  <!-- end movie-images -->
 
 
 
