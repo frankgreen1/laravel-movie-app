a3fdfc9d3ec573ec41019dce0d7bfd68a4a9ceb5
greenfrank1 <greenfrank010@gmail.com>
2021-03-14 19:13:47 +0100

    rewrote the entire show.blade code

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -11,58 +11,80 @@
                    <div class="ml-24">
                        <h2 class="text-4xl mt-4 md:mt-0 font-semibold">{{ $show['title']}}</h2>
                        <div class="flex flex-wrap items-center text-gray-400 text-sm mt-1">
-                        <svg class="fill-current text-red-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg>
-                        <span class="ml-1">{{$show['vote_average'] *10 .'%'}}</span>
-                        <span class="mx-2">|</span>
-                        <span>{{\Carbon\Carbon::parse($show['release_date'])->format('M d,Y')}}</span>
-                        <span class="mx-2">|</span>
-                        <span class="mx-2">
-                            @foreach ($show['genres'] as $genre)
-                            {{ $genre['name']}} @if (! $loop->last),@endif
-                            @endforeach
-                        </span>
+                            <svg class="fill-current text-red-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg>
+                            <span class="ml-1">{{$show['vote_average'] *10 .'%'}}</span>
+                            <span class="mx-2">|</span>
+                            <span>{{\Carbon\Carbon::parse($show['release_date'])->format('M d,Y')}}</span>
+                            <span class="mx-2">|</span>
+                            <span class="mx-2">
+                                @foreach ($show['genres'] as $genre)
+                                {{ $genre['name']}} @if (! $loop->last),@endif
+                                @endforeach
+                            </span>
                        </div>
                        <p class="text-gray-400 mt-8">
                           {{ $show['overview']}}
                        </p>
-                       <div class="mt-12">
-                           <h4 class="text-white font-semibold">Featured Cast</h4>
-                           <div class="flex mt-4">
-                                @foreach ($show['credits']['crew'] as $crew)
-                                    @if ($loop->index <2 )
-                                        <div class="mr-8">
-                                            <div>
-                                                {{ $crew['name']}}
+
+                        <div class="mt-12">
+                            <h4 class="text-white font-semibold">Featured Cast</h4>
+                            <div class="flex mt-4">
+                                    @foreach ($show['credits']['crew'] as $crew)
+                                        @if ($loop->index <2 )
+                                            <div class="mr-8">
+                                                <div>
+                                                    {{ $crew['name']}}
+                                                </div>
+                                                <div class="text-sm text-gray-400">
+                                                {{ $crew['job']}}
+                                                </div>
                                             </div>
-                                            <div class="text-sm text-gray-400">
-                                            {{ $crew['job']}}
-                                            </div>
-                                        </div>
-                                    @endif
-                                @endforeach
+                                        @endif
+                                    @endforeach
 
 
-                           </div>
-                       </div>
-                       <div x-data="{ isOpen: false }">
-                             @if (count($show['videos']['results'])> 0)
+                            </div>
+                        </div>
+
+                        <div x-data ="{ isOpen: false}">
+                            @if(count($show['videos']['results']) > 0)
                                 <div class="mt-12">
-                                <button
-                                    @click="isOpen = true"
 
-                                    class="flex inline-flex items-center bg-red-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-red-600
-                                            transition ease-in-out duration-150"
+                                    <button
+                                        @click="isOpen = true"
+                                        class="flex inline-flex items-center bg-red-500 text-gray-900 round font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150"
                                     >
-                                            <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48
-                                            2 12s4.48 10 10 10 10-4.48 10-10S17.52 2
-                                            12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
-                                            </svg>
-                                            <span class="ml-2">Play Trailer</span>
-                                </button>
+                                        <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+                                        <span class="ml-2">play Trailer</span>
+                                    </button>
+
                                 </div>
                             @endif
+                            <div
+                               style="background-color: rgba(0, 0, 0, .5);"
+                                class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                                x-show="isOpen">
+                                <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                                    <div class="bg-gray-900 rounded">
+                                        <div class="flex justify-end pr-4 pt-2">
+                                            <button
+                                              @click="isOpen = false"
+                                              class="text-3xl,loading-none hover:text-gray-300">&times;
 
-                       </div>
+                                            </button>
+                                        </div>
+
+                                    </div>
+                                    <div class="modal-body px-8 py-8">
+                                        <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
+                                            <iframe class="responsive-iframe absolute top-0 left-0 w-full h-full" src="https://www.youtube.com/embed/{{ $show['videos']['results'][0]['key'] }}" style="border:0;" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
                    </div>
               </div>
      </div><!-----end movie info----->
@@ -110,30 +132,33 @@
                        @endif
                 @endforeach
             </div>
-                           <div
-                               style="background-color: rgba(0, 0, 0, .5);"
-                               class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
-                               x-show ="isOpen"
-                           >
-                               <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
-                                   <div class="bg-gray-900 rounded">
-                                       <div class="flex justify-end pr-4 pt-2">
-                                           <button
-                                           @click="isOpen = false"
-                                               @keydown.escape.window="isOpen = false"
-                                               class="text-3xl leading-none hover:text-gray-300">&times;
-                                           </button>
-                                       </div>
-                                       <div class="modal-body px-8 py-8">
+            <div
+                style="background-color: rgba(0, 0, 0, .5);"
+                class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                x-show ="isOpen"
+             >
+                <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                    <div class="bg-gray-900 rounded">
+                        <div class="flex justify-end pr-4 pt-2">
+                            <button
+                            @click="isOpen = false"
+                                @keydown.escape.window="isOpen = false"
+                                class="text-3xl leading-none hover:text-gray-300">&times;
+                            </button>
+                        </div>
+                        <div class="modal-body px-8 py-8">
 
-                                               <img :src="image" alt="poster">
+                                <img :src="image" alt="poster">
 
-                                       </div>
-                                   </div>
-                               </div>
-                           </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>  <!-- end movie-images -->
+
+
 
 
 
